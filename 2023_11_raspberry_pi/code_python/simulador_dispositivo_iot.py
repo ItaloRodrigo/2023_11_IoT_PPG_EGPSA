37ee1370fa9ae4d28fae8b7d7c86f03b083c27ae
Ítalo Rodrigo <italo.computation@gmail.com>
2023-11-01 21:04:01 -0400

    Update simulador python
    Update simulador python

--- 2023_11_raspberry_pi/code_python/simulador_dispositivo_iot.py
+++ 2023_11_raspberry_pi/code_python/simulador_dispositivo_iot.py
@@ -1,6 +1,6 @@
 #Programa: simulador de dispositivo IoT.
 #          Plataforma IoT usada: Tago.io
-#Autor: Pedro Bertoleti
+#Autor: Ítalo Rodrigo
 #
 #Importante: rode o script utilizando Python 2.x:
 #python simulador_dispositivo_iot.py
@@ -12,7 +12,7 @@ import time
 import json
 
 # Definicoes:
-device_token = " " #device token do seu dispositivo
+device_token = "75d1e803-d751-4372-84e5-dd92025d54eb" #device token do seu dispositivo
 broker = "mqtt.tago.io"
 porta_broker = 1883
 keep_alive_broker = 60
@@ -23,14 +23,14 @@ mqtt_password = device_token
 
 # Callback - conexao ao Broker realizada
 def on_connect(client, userdata, flags, rc):
-    print "[STATUS] Conectado ao Broker. Resultado de conexao: " + str(rc) 
+    print ("[STATUS] Conectado ao Broker. Resultado de conexao: " + str(rc)) 
     
     #faz subscribe automatico no topico
     client.subscribe(topico_publish)
 
 # Programa principal
 try:
-    print "[STATUS] Inicializando MQTT..." 
+    print ("[STATUS] Inicializando MQTT...") 
     client = mqtt.Client() #inicializa MQTT:
     client.username_pw_set(mqtt_username, mqtt_password)
     client.on_connect = on_connect
@@ -45,8 +45,8 @@ try:
         # Gera temperatura e umidade aleatorias
         temperatura = random.randint(20,25)
         umidade = random.randint(50,60)
-        print "Temperatura gerada: " + str(temperatura) + "C" 
-        print "Umidade gerada: " + str(umidade) + "%" 
+        print ("Temperatura gerada: " + str(temperatura) + "C") 
+        print ("Umidade gerada: " + str(umidade) + "%") 
             
         # Prepara a formatacao dos dados coletados
         temperatura_json = {"variable": "temperatura", "unit": "C", "value": temperatura}
@@ -63,5 +63,5 @@ try:
 
 # Encerramento do programa (pressiona CTRL+C)
 except KeyboardInterrupt:
-    print  "\nCtrl+C pressionado, encerrando aplicacao e saindo..." 
+    print  ("\nCtrl+C pressionado, encerrando aplicacao e saindo...") 
     sys.exit(0)
