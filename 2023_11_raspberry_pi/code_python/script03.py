998222212fc9dba69caaaa7899b9504ae3efc5cf
Ítalo Rodrigo <italo.computation@gmail.com>
2023-11-02 23:35:28 -0400

    Create script03.py
    Create script03.py

---
+++ 2023_11_raspberry_pi/code_python/script03.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+import json
+ 
+url = "https://api.tago.io/data"
+
+API_KEY = "932c43e0-91a1-47ad-8e99-790ed9659aba"
+ 
+headers = {
+    "Content-Type": "application/json; charset=utf-8",
+    "Device-Token": API_KEY,
+    "ssl": "false",
+}
+
+try:
+    while True:
+        
+        data = [
+            {"variable": "temperatura","value": "50"},
+            {"variable": "umidade","value": "20"},
+        ]
+    
+        response = requests.post(url, headers=headers, json=data)
+        
+        print("Status Code", response.status_code)
+        print("JSON Response ", response.json())    
+except KeyboardInterrupt:
+    print ("dfhfdh")
+    sys.exit(0)
+
