8014f2097b1617feb36959b053a0ed514867d558
Ítalo Rodrigo <italo.computation@gmail.com>
2023-11-10 21:53:18 -0400

    updates
    updates

---
+++ 2023_11_raspberry_pi/exercicios/function_random.py
@@ -0,0 +1,15 @@
+import random
+
+lista = [
+    {"equipe 01": "Top Team"},
+    {"equipe 02": "Sistema de irrigação em plantação de milho com auxílio do Raspberry PI"},
+    {"equipe 03": "PORAQUÊ 1.0"},
+    {"equipe 04": "China"},
+    {"equipe 05": "Por hoje"},
+    {"equipe 06": "Electrical Static"}
+]
+
+print("A equipe sorteada foi ")
+print(random.choice(lista))
+
+
